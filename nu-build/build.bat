aaa7d6bf005a88db4d6443a6e643daace018e97b
Xavier Jefferson <xavierj@autobytel.com>
2018-01-10 11:02:07 -0800

    Add nuspec for fluentnhibernatetools

--- nu-build/build.bat
+++ nu-build/build.bat
@@ -1,3 +1,4 @@
 mkdir .\packages
+..\.nuget\nuget.exe pack ..\nuspecs\Snork.FluentNHibernateTools.nuspec -outputdirectory .\packages
 ..\.nuget\nuget.exe pack ..\nuspecs\Log4Net.FluentNHibernateAppender.nuspec -outputdirectory .\packages
 pause
